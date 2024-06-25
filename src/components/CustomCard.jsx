4828c82f032264be066019639920a416078c1a86
Isabela Santos <isa2014mgspn@gmail.com>
2024-06-24 21:13:33 -0300

    feat: add card pagina inicial

---
+++ src/components/CustomCard.jsx
@@ -0,0 +1,47 @@
+// src/components/CustomCard.jsx
+import React from 'react';
+import { Card, CardContent, Typography, Box } from '@mui/material';
+import { styled } from '@mui/system';
+
+const CustomCard = styled(Card)({
+  borderRadius: '8px',
+  backgroundColor: '#F6F6F6',
+  display: 'flex',
+  alignItems: 'center',
+  padding: '16px',
+  boxShadow: 'none',
+  height: '200px', // Defina a altura desejada para o card
+});
+
+const ImageBox = styled(Box)(({ image }) => ({
+  backgroundImage: `url(${image})`,
+  backgroundSize: 'contain',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'left',
+  width: '109px', 
+  height: '134px', 
+  flex: '0 0 auto',
+  marginRight: '16px',
+}));
+
+const ContentBox = styled(CardContent)({
+  flex: '1 1 auto',
+});
+
+const CustomCardComponent = ({ title, text, image }) => {
+  return (
+    <CustomCard>
+      <ImageBox image={image} />
+      <ContentBox>
+        <Typography  component="div" style={{ fontFamily: 'Montserrat', fontSize:'16px', color:'#777777', fontWeight:'600'}}>
+          {title}
+        </Typography>
+        <Typography   style={{ fontFamily: 'Montserrat', fontSize:'12px', color:'#777777',  fontWeight:'400'}}>
+          {text}
+        </Typography>
+      </ContentBox>
+    </CustomCard>
+  );
+};
+
+export default CustomCardComponent;
